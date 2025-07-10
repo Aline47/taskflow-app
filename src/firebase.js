ce00c730157808a646f72b808440eb0f893c0ea2
Aline47 <alinepruebareyes@gmail.com>
2025-07-10 12:21:06 -0600

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Importa las funciones que necesitas de los SDKs que necesitas
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Añade la configuración de tu proyecto de Firebase aquí
+const firebaseConfig = {
+  apiKey: "AIzaSyDEK-mh-_Om83U8Ov_j3MprTnCqUAtc0OQ",
+  authDomain: "asignacion-de-tareas.firebaseapp.com",
+  projectId: "asignacion-de-tareas",
+  storageBucket: "asignacion-de-tareas.firebasestorage.app",
+  messagingSenderId: "458090747767",
+  appId: "1:458090747767:web:894bcf8ee5444cf8ca7372",
+  measurementId: "G-EK65W6M4ZV"
+};
