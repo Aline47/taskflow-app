9668ac17fade95ed5bc3fd2a15edd1502fbcee22
Aline47 <alinepruebareyes@gmail.com>
2025-07-10 12:24:35 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,3 +12,9 @@ const firebaseConfig = {
   appId: "1:458090747767:web:894bcf8ee5444cf8ca7372",
   measurementId: "G-EK65W6M4ZV"
 };
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// LA LÍNEA MÁS IMPORTANTE: Exporta la base de datos
+// Asegúrate de que la palabra "export" esté aquí.
+export const db = getFirestore(app);
