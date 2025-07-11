81f0b754a548a4c8c70bb3d8cb4269c822b68bbe
Aline47 <alinepruebareyes@gmail.com>
2025-07-10 22:30:31 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  darkMode: "class", // Habilita el modo oscuro basado en clases
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
